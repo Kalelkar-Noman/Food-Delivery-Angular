fcbacaca23c4d4995d581c503189d1e7f3a7d568
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2024-07-04 13:59:07 +0530

    Enabled Netlify forms for contact-us page

--- src/app/services/api-cache.service.ts
+++ src/app/services/api-cache.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiCacheService {
+  constructor() {}
 
-  constructor() { }
+  private cache: Map<string, any> = new Map();
+
+  get(key: string) {
+    return this.cache.get(key);
+  }
+
+  public set(key: string, value: any): void {
+    this.cache.set(key, value);
+  }
 }
